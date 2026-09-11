5daf5b83b7bc2d8c703bd695a50bdeaefdae2c77
MACKA SARR <mackasarr27@gmail.com>
2026-09-11 11:18:53 +0200

    fix: appointment email notification

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function HeroBanner() {
   return (
     <section className="w-full overflow-hidden">
-      <div className="relative isolate h-[52svh] min-h-[360px] sm:h-[66svh] sm:min-h-[480px] lg:h-[82svh] lg:min-h-[680px]">
+      <div className="relative isolate h-[38svh] min-h-[300px] max-h-[340px] sm:h-[66svh] sm:min-h-[480px] sm:max-h-none lg:h-[82svh] lg:min-h-[680px]">
         <Image
           src="/Bannière.png"
           alt="Bannière An Nour"
@@ -14,7 +14,7 @@ export default function HeroBanner() {
           priority
           quality={100}
           sizes="100vw"
-          className="h-full w-full object-contain object-center sm:object-contain"
+          className="h-full w-full object-cover object-center sm:object-contain"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/25 to-transparent" />
         <div className="absolute inset-x-0 bottom-0 flex items-end justify-center px-3 pb-3 sm:px-4 sm:pb-8 lg:pb-12">
